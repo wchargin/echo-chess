173b6ede33ecc9564b78ce6edba6a6cbaaf7c2c3
Willow Chargin <wchargin@gmail.com>
2023-08-30 21:22:38 -0700

    engine: add basic stepper movement for pawns

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,95 @@
+struct Pawn;
+
+/// A subset of the squares on a chess board.
+///
+/// Square in file `x` and rank `y` is indicated by bit `8 * y + x`. For instance, B1 is bit `1`
+/// and A2 is bit `8`.
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+struct SquareSet(u64);
+
+impl SquareSet {
+    fn draw(self: SquareSet) -> String {
+        let mut res = String::new();
+        for y in (0..8).rev() {
+            res.push(char::from_u32('a' as u32 + y).unwrap());
+            res.push(' ');
+            for x in 0..8 {
+                if self.0 & (1 << (8 * y + x)) != 0 {
+                    res.push('*');
+                } else {
+                    res.push('.');
+                }
+            }
+            res.push('\n');
+        }
+        res.push_str("  12345678\n");
+        res
+    }
+}
+
+impl std::ops::BitAnd<SquareSet> for SquareSet {
+    type Output = SquareSet;
+    fn bitand(self, rhs: SquareSet) -> SquareSet {
+        SquareSet(self.0 & rhs.0)
+    }
+}
+impl std::ops::BitOr<SquareSet> for SquareSet {
+    type Output = SquareSet;
+    fn bitor(self, rhs: SquareSet) -> SquareSet {
+        SquareSet(self.0 | rhs.0)
+    }
+}
+impl std::ops::Not for SquareSet {
+    type Output = SquareSet;
+    fn not(self) -> SquareSet {
+        SquareSet(!self.0)
+    }
+}
+impl std::ops::Shl<u32> for SquareSet {
+    type Output = SquareSet;
+    fn shl(self, rhs: u32) -> SquareSet {
+        SquareSet(self.0 << rhs)
+    }
+}
+impl std::ops::Shr<u32> for SquareSet {
+    type Output = SquareSet;
+    fn shr(self, rhs: u32) -> SquareSet {
+        SquareSet(self.0 >> rhs)
+    }
+}
+
+trait Stepper {
+    fn move_steps(from: SquareSet) -> SquareSet;
+    fn capture_steps(from: SquareSet) -> SquareSet;
+}
+
+fn captures<S: Stepper>(from: SquareSet, obstacles: SquareSet, targets: SquareSet) -> SquareSet {
+    let permeable = !(obstacles | targets);
+    let mut reachable = from & permeable;
+    loop {
+        let next = (reachable | S::move_steps(reachable)) & permeable;
+        if next == reachable {
+            break;
+        }
+        reachable = next;
+    }
+    S::capture_steps(reachable) & targets
+}
+
+impl Stepper for Pawn {
+    fn move_steps(from: SquareSet) -> SquareSet {
+        SquareSet(from.0 << 8)
+    }
+    fn capture_steps(from: SquareSet) -> SquareSet {
+        let to_left = from & SquareSet(0xfefefefefefefefe);
+        let to_right = from & SquareSet(0x7f7f7f7f7f7f7f7f);
+        (to_left << 7) | (to_right << 9)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let start = SquareSet(0x8040201008040201);
+    println!("start:\n{}", start.draw());
+    println!("pawn steps:\n{}", Pawn::move_steps(start).draw());
+    println!("pawn captures:\n{}", Pawn::capture_steps(start).draw());
 }
